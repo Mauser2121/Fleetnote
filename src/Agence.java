f8d0fb448b1abbdb8e3becb79b6f377f85b1da8c
jamel_benmohamed <jamel.benmohamed@direct-fleet.com>
2013-10-23 18:24:08 +0200

    Scrap Rentacar Ucar

---
+++ src/Agence.java
@@ -0,0 +1,53 @@
+
+public class Agence {
+
+	private String nom;
+	private String adresse;
+	private String codePostale;
+	private String ville;
+	private String téléphone;
+	private String email;
+	
+	public Agence(){
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getCodePostale() {
+		return codePostale;
+	}
+	public void setCodePostale(String codePostale) {
+		this.codePostale = codePostale;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getTéléphone() {
+		return téléphone;
+	}
+	public void setTéléphone(String téléphone) {
+		this.téléphone = téléphone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
